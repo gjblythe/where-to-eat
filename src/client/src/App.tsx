6a019a2ddc17d3c8057e7f3f43a4236b3ea33494
Garrett <gjblythe@gmail.com>
2020-10-09 14:03:32 -0700

    remove unused var

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import Routes from './components/Routes';
-import Search from './components/search/Search';
-import Settings from './components/settings/Settings';
 
 export default () => {
   return (
