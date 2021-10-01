a8ce3827329d3e0e225209868bff2cb604fdb2ff
Garrett <gjblythe@gmail.com>
2021-10-01 14:55:06 -0700

    theme

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,38 +1,47 @@
 import firebase from 'firebase';
-import { createMuiTheme, CssBaseline, ThemeProvider, useMediaQuery } from '@material-ui/core';
-import cyan from '@material-ui/core/colors/cyan';
+import { createMuiTheme, CssBaseline, ThemeProvider } from '@material-ui/core';
 import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import Routes from './components/Routes';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { firebaseConfig } from './firebaseConfig';
 
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
-const firestore = firebase.firestore();
+
+export const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
 export default () => {
   const [user, loading] = useAuthState(auth)
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const [mode, setMode] = React.useState<'light' | 'dark'>('light');
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+
   const theme = React.useMemo(
     () =>
-      createMuiTheme({
+    createMuiTheme({
         palette: {
-          type: prefersDarkMode ? 'dark' : 'light',
-          primary: cyan,
+          type: mode
         },
       }),
-    [prefersDarkMode],
+    [mode],
   );
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline/>
-      <Router>
-        <Routes user={user} loading={loading}/>
-      </Router>
-    </ThemeProvider>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Router>
+          <Routes user={user} loading={loading}/>
+        </Router>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 };
